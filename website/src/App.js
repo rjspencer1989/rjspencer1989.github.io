7d359b3eea647f088cf43c1962b013af7cb29bbf
Robert Spencer <rob@robspencer.me.uk>
2021-08-07 17:48:42 +0100

    updates for react

--- website/src/App.js
+++ website/src/App.js
@@ -1,29 +1,45 @@
 import React from 'react';
-import {HashRouter, Route, Link} from 'react-router-dom';
-import logo from './logo.svg';
+import {HashRouter, Link, Route} from 'react-router-dom';
 import './App.css';
+import {Nav, Navbar, NavItem} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
+import About from "./about";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <HashRouter basename={'/'}>
-          <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                  Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                  Learn React
-              </a>
-          </header>
-      </HashRouter>
+    <div className="App container">
+        <Navbar expand="md">
+            <Navbar.Header>
+                <Navbar.Brand><Link to={"/"}> Rob Spencer</Link></Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+
+            <Navbar.Collapse>
+                <Nav>
+                    <LinkContainer to={"/"}>
+                        <NavItem>Home</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to={"#about"}>
+                        <NavItem>About</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to={"#software"}>
+                        <NavItem>Software</NavItem>
+                    </LinkContainer>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      {/*<HashRouter hashType={"noslash"}>*/}
+      {/*    <div>*/}
+      {/*        <Route exact path="/" component={Home} />*/}
+      {/*        <Route path="#about" component={About} />*/}
+      {/*        <Route path="#software" component={Software} />*/}
+      {/*    </div>*/}
+      {/*</HashRouter>*/}
     </div>
   );
 }
+
+// const Home = () => <div><h2>Home</h2></div>;
+//     const Software = () => <div><h2>Software</h2></div>;
 
 export default App;
---
+++ website/src/about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = (props) => {
+    return (
+     <div>
+         <h1>About Me</h1>
+         <p>I currently work in the 5G Innovation Centre at the University of Surrey.</p>
+
+         <p>My PhD focused specifically on networking usability issues.</p>
+     </div>
+    )
+};
+
+export default About;
